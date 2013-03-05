106a75f366f6bdcb5abb7fd332239dc6ef9dc691
Christopher Lopes <christopher.lopes@gmail.com>
2013-03-04 21:53:42 -0800

    Create Lopes pedigree

---
+++ source/javascripts/data.js
@@ -0,0 +1,230 @@
+var json = {
+	"name": "Inacio Pedro Lopes", "birth": "09 Aug 1869 Corvo", "married":"10 Jan 1907 Corvo", "death":"New Bedford",
+	"children": [
+		{
+			"name": "Inacio Pedro Coelho","birth":"27 Jan 1827 Corvo","married":"29 May 1851 Corvo","death":"23 Jul 1907 Corvo",
+			"children": [
+				{
+					"name": "Manuel Francisco Mendes","birth":"10 Dec 1790 Corvo","married":"28 May 1820 Corvo","death":"20 Jun 1870 Corvo",
+					"children": [
+						{
+							"name": "Francisco Lourenco Mendes","birth":"25 Sep 1754 Corvo","married":"20 Dec 1789 Corvo","death":"22 Mar 1842 Corvo",
+							"children": [
+								{
+									"name": "Antonio Lourenco Mendes","birth":"About 1710 Corvo","married":"20 Feb 1735 Corvo","death":"25 Feb 1788 Corvo",
+									"children": [
+										{
+											"name": "Francisco Lourenco","birth":"13 Mar 1672 Corvo","married":"01 May 1698 Corvo","death":"21 Dec 1742 Corvo",
+											"children": [
+												{
+													"name": "Capt. Valentim Lourenco","birth":"About 1650","married":"About 1670","death":"17 Jan 1706 Corvo",
+													"children": [
+														{"name": "Balthazar Lourenco","birth":"About 1620","married":"Before 1650","death":"11 Apr 1698 Corvo"},
+														{"name": "Ana Pimentel","birth":"About 1630","married":"","death":"01 Mar 1704 Corvo"}
+													]
+												},
+												{"name": "Catarina Coelha","birth":"About 1640","married":"","death":"10 Mar 1684 Corvo"}
+											]
+										},
+										{
+											"name": "Maria Rodrigues","birth":"About 1680 Corvo","married":"","death":"22 Jul 1719 Corvo",
+											"children": [
+												{"name": "Manuel Rodrigues Ramos","birth":"About 1640 Ponta Delgada","married":"05 Jun 1681 Ponta Delgada","death":"26 Oct 1697 Corvo"},
+												{
+													"name": "Esperanca Ferreira Rodrigues","birth":"About 1650 Corvo","married":"","death":"24 Nov 1703 Corvo",
+													"children": [
+														{"name": "Goncalo Ferreira","birth":"About 1620","married":"Befor 1650","death":""},
+														{"name": "Catarina Heitor","birth":"About 1620","married":"","death":""}
+													]
+												}
+											]
+										}
+									]
+								},
+								{
+									"name": "Isabel de Avelar","birth":"1717 Corvo","married":"","death":"26 May 1765 Corvo",
+									"children": [
+										{
+											"name": "Balthazar de Avelar","birth":"28 Dec 1662 Corvo","married":"19 Jun 1692 Ponta Delgada","death":"10 Feb 1735 Corvo",
+											"children": [
+												{"name": "Francisco de Avelar","birth":"About 1626","married":"","death":"Before 1656"},
+												{"name": "Barbara Valadao","birth":"About 1636","married":"","death":"01 Jan 1696"}
+											]
+										},
+										{
+											"name": "Cecelia de Freitas","birth":"About 1671 Ponta Delgada Santa Cruz das Flores","married":"","death":"24 Aug 1739 Corvo",
+											"children": [
+												{"name": "Manuel de Fraga","birth":"Between 1611-1620 Ponta Delgada Santa Curz das Flores","married":"About 1650","death":"Between 1675-1685"},
+												{"name": "Barbara Henriques","birth":"1630 Ponta Delgada Santa Cruz das Flores","married":"","death":"29 Nov 1681 Ponta Delgada Flores"}
+											]
+										}
+									]
+								}
+							]
+						},
+						{
+							"name": "Isabel Coelha","birth":"About 1761 Corvo","married":"","death":"Before 1818 Corvo",
+							"children": [
+								{"name": "Francisco Coelho da Volta","birth":"About 1720 Corvo","married":"Before 1750 Corvo","death":""},
+								{"name": "Maria Coelha","birth":"Abount 1720 Corvo","married":"","death":""}
+							]
+						}
+					]
+				},
+				{
+					"name": "Maria da Encarnacao","birth":"25 May 1788 Corvo","married":"","death":"22 Feb 1865 Corvo",
+					"children": [
+						{
+							"name": "Lt Manoel Coelho Rodrigues","birth":"21 Jan 1756 Corvo","married":"26 Spe 1784 Corvo","death":"04 Aug 1833 Corvo",
+							"children": [
+								{
+									"name": "Capitao Manuel Coelho Rodrigues","birth":"22 Mar 1719 Corvo","married":"23 Dec 1736 Corvo","death":"After 1783",
+									"children": [
+										{"name": "Pai Unknown","birth":"Abount 1680","married":"","death":""},
+										{"name": "Luzia Coelha","birth":"About 1680","married":"","death":"Before 23 Dec 1736"}
+									]
+								},
+								{
+									"name": "Maria Valadao","birth":"08 Jan 1724 Corvo","married":"","death":"15 Dec 1774 Corvo",
+									"children": [
+										{
+											"name": "Joao Valadao","birth":"About 1690","married":"27 Feb 1718 Corvo","death":"23 Oct 1754 Corvo",
+											"children": [
+												{"name": "Antonio Valadao","birth":"About 1660","married":"Before 1690","death":""},
+												{"name": "Maria Coelha","birth":"About 1660","married":"","death":""}
+											]
+										},
+										{
+											"name": "Maria Coelha","birth":"About 1695 corvo","married":"","death":"28 Nov 1760 Corvo",
+											"children": [
+												{
+													"name": "Joao Coelho Homen","birth":"Sep 1671 Corvo","married":"Before 1690","death":"17 Dec 1739 Corvo",
+													"children": [
+														{"name": "Manuel Homen","birth":"About 1640","married":"10 Sep 1662 Corvo","death":"15 Oct 1698 Corvo"},
+														{"name": "Isabel Coelha","birth":"About 1640","married":"","death":"06 May 1680 Corvo"}
+													]
+												},
+												{"name": "Maria Rodrigues","birth":"About 1670","married":"","death":"09 Mar 1729 Corvo"}
+											]
+										}
+									]
+								}
+							]
+						},
+						{
+							"name": "Maria Valada da Encarnaco","birth":"23 Mar 1752 Corvo","married":"","death":"Before Jul 1828 Corvo",
+							"children": [
+								{
+									"name": "Joao Coelho Valadao","birth":"28 August 1722 Corvo","married":"13 Jan 1743 Corvo","death":"25 Sep 1783 Corvo",
+									"children": [
+										{
+											"name": "Manuel Coelho Valadao","birth":"About 1680 Corvo","married":"12 sep 1717 Corvo","death":"22 Nov 1760",
+											"children": [
+												{"name": "Manuel Coelho","birth":"About 1650","married":"Before 1680","death":"26 Nov 1716 Corvo"},
+												{"name": "Ana Coelha","birth":"About 1650","married":"","death":"18 Aug 1708 Corvo"}
+											]
+										},
+										{
+											"name": "Ana Coelha","birth":"About 1690 Corvo","married":"","death":"03 Jus 1731 Corvo",
+											"children": [
+												{"name": "Manuel Pimentel","birth":"About 1650","married":"Before 1680","death":"17 Oct 1737 Corvo"},
+												{"name": "Agada Coelha","birth":"About 1650","married":"","death":"Before 1737"}
+											]
+										}
+									]
+								},
+								{"name": "Josefa Coelha","birth":"Before 1717 Corvo","married":"","death":"02 Apr 1788 Corvo"}
+							]
+						}
+					]
+				}
+			]
+		},
+		{
+			"name": "Ana de Jesus da Encarnaco Lopes","birth":"01 Jan 1832 Corvo","married":"","death":"After 1907",
+			"children": [
+				{
+					"name": "Capt. Joaqum Pedro Coelho","birth":"22 Sep 1798 Corvo","married":"01 Jul 1828 Corvo","death":"29 Oct 1836 Corvo",
+					"children": [
+						{
+							"name": "Lt Manoel Coelho Rodrigues","birth":"21 Jan 1756 Corvo","married":"26 Sep 1784 Corvo","death":"04 Aug 1833 Corvo",
+							"children": [
+								{
+									"name": "Capitao Manuel Coelho Rodrigues","birth":"22 Mar 1719 Corvo","married":"23 Dec 1736 Corvo","death":"After 1783",
+									"children": [
+										{"name": "Pai Unknown","birth":"About 1680","married":"","death":""},
+										{"name": "Luzia Coelha","birth":"About 1680","married":"","death":"Before 23 Dec 1736"}
+									]
+								},
+								{
+									"name": "Maria Valadao","birth":"","married":"","death":"",
+									"children": [
+										{
+											"name": "Joao Valadao","birth":"About 1690","married":"27 Feb 1718 Corvo","death":"23 Oct 1754 Corvo",
+											"children": [
+												{"name": "Antonio Valadao","birth":"About 1660","married":"Before 1690","death":""},
+												{"name": "Maria Coelha","birth":"About 1660","married":"","death":""}
+											]
+										},
+										{
+											"name": "Maria Coelha","birth":"About 1695 Corvo","married":"","death":"28 Dov 1760 Corvo",
+											"children": [
+												{
+													"name": "Joao Coelho Homen","birth":"Sep 1671 Corvo","married":"Before 1690","death":"17 Dec 1739 Corvo",
+													"children": [
+														{"name": "Manuel Homen","birth":"About 1640","married":"10 Sep 1662 Corvo","death":"15 Oct 1698 Corvo"},
+														{"name": "Isabel Coelha","birth":"About 1640","married":"","death":"06 May 1680 Corvo"}
+													]
+												},
+												{"name": "Maria Rodrigues","birth":"About 1670","married":"","death":"09 Mar 1729 Corvo"}
+											]
+										}
+									]
+								}
+							]
+						},
+						{
+							"name": "Maria Valadao da Encarnacao","birth":"23 Mar 1752 Corvo","married":"","death":"Before Jul 1828 Corvo",
+							"children": [
+								{
+									"name": "Joao Coelho Valadao","birth":"28 Agu 1722 Corvo","married":"13 Jan 1743 Corvo","death":"25 Sep 1783 Corvo",
+									"children": [
+										{
+											"name": "Manuel Coelho Valadao","birth":"About 1680 Corvo","married":"12 Sep 1717 Corvo","death":"22 Nov 1760 Corvo",
+											"children": [
+												{"name": "Manuel Coelho Coelho","birth":"About 1650","married":"Before 1680","death":"26 Nov 1716 Corvo"},
+												{"name": "Ana Coelha","birth":"About 1650","married":"","death":"18 Aug 1708 Corvo"}
+											]
+										},
+										{
+											"name": "Ana Coelha","birth":"About 1690 Corvo","married":"","death":"03 Jul 1731 Corvo",
+											"children": [
+												{"name": "Manuel Pimentel","birth":"About 1650","married":"Before 1680","death":"17 Oct 1737 Corvo"},
+												{"name": "Agada Coelha","birth":"","married":"About 1650","death":"Before 1737"}
+											]
+										}
+									]
+								},
+								{"name": "Josefa Coelha","birth":"Before 1718 Corvo","married":"","death":"02 Apr 1788 Corvo"}
+							]
+						}
+					]
+				},
+				{
+					"name": "Marianna de Jesus","birth":"About 1800","married":"","death":"After 1856",
+					"children": [
+						{"name": "Pai Incognito","birth":"About 1780","married":"Before 1800","death":""},
+						{"name": "Francisca Rosa de Jesus","birth":"About 1780","married":"","death":""}
+					]
+				}
+			]
+		}
+	]
+}
+
+// {
+//   "name": "something",
+//   "children": [
+//     {"name": "other thing"}
+//   ]
+// }
